ddd073612f9f9571639c1156e9f5781274defc57
ManonMartin <manon@mac-332-180.local>
2020-12-17 10:38:38 +0100

    updated yml

--- script/Hello.R
+++ script/Hello.R
@@ -1,11 +1,10 @@
 # setwd("/Users/manon/Documents/CBIO/Packages/documentation/hpar/gh_actions/test_GHaction")
 
+load(file.path("data","hpaRelease.rda"))
   
-  load(file.path("data","hpaRelease.rda"))
+### testing hpar version
   
-  ### testing hpar version
-  
-  getHpaRelease <- function() {
+getHpaRelease <- function() {
     rel <- readLines("http://www.proteinatlas.org/about/releases", warn = FALSE)
     suppressWarnings(reldate <- grep("Release date:", rel, value = TRUE)[1])
     suppressWarnings(relver <- grep("Protein Atlas version", rel, value = TRUE)[1])
@@ -19,22 +18,23 @@
     return(ans)
   }
   
-  HPARel <- getHpaRelease()
+HPARel <- getHpaRelease()
   
-  mat <- rbind(HPARel, inst_package = c(hpaRelease["version"], 
+mat <- rbind(HPARel, inst_package = c(hpaRelease["version"], 
                                         hpaRelease["date"],
                                         hpaRelease["ensembl"]))
   
-  ### is hpar up to date?
+### is hpar up to date?
   
-  test <- apply(mat,2, duplicated)[2,]
+test <- apply(mat,2, duplicated)[2,]
   
-  if (sum(test)==3){
-    txt_hpar <- "<p> Everything is up-to-date for <code> hpar </code> </p>"
-  } else{
+if (sum(test)==3){
+  "Everything is up-to-date for hpar"
+  warning(txt_hpar)
+  }else{
     ix <- !test
-    txt_hpar <-  paste0("<p> A new version of HPA is available: <br> " , paste0(names(mat["hpaRelease",ix]),": ",
-                        mat["hpaRelease",ix], collapse = " <br>"), "</p>")
+    txt_hpar <-  paste0("A new version of HPA is available: " , paste0(names(mat["hpaRelease",ix]),": ",
+                        mat["hpaRelease",ix], collapse = " "), "")
+    warning(txt_hpar)
   }
   
-  txt_hpar
