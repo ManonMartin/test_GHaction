2d204901896f7112c2fa82a31b3b3e72f16ebc1b
ManonMartin <manon@mac-332-180.local>
2020-12-17 10:27:06 +0100

    updated yml

--- script/Hello.R
+++ script/Hello.R
@@ -1,56 +1,40 @@
+# setwd("/Users/manon/Documents/CBIO/Packages/documentation/hpar/gh_actions/test_GHaction")
 
-library(emayili)
-library(statquotes)
-library(magrittr)
-library(hpar)
-
-email <- envelope(
-  to = "manon.martin@uclouvain.be",
-  from = "manon.martin@uclouvain.be",
-  subject = "HPA version update"
-)
-
-### testing hpar version
-
-getHpaRelease <- function() {
-  rel <- readLines("http://www.proteinatlas.org/about/releases", warn = FALSE)
-  suppressWarnings(reldate <- grep("Release date:", rel, value = TRUE)[1])
-  suppressWarnings(relver <- grep("Protein Atlas version", rel, value = TRUE)[1])
-  reldate <- sub("</b.+$", "", sub("^.+<b>", "", reldate))
-  relver <- sub("<.+$", "", sub("^.+version ", "", relver))
-  hpa <- c("version" = relver, "date" = reldate)
-  ens <- grep("Ensembl version", rel, value = TRUE, useBytes=TRUE)[1]
-  ens <- sub("</b>", "", sub("^.+<b>", "", ens))
-  ens <- sub("\t", "", ens)
-  ans <- c(hpa, ensembl = ens)
-  return(ans)
-}
-
-hpaRelease <- getHpaRelease()
-
-mat <- rbind(hpaRelease, inst_package = c(getHpaVersion(), getHpaDate(),
-                    getHpaEnsembl()))
-
-### is hpar up to date?
-
-test <- apply(mat,2, duplicated)[2,]
-
-if (sum(test)==3){
-  txt_hpar <- "<p> Everything is up-to-date for <code> hpar </code> </p>"
-} else{
-  ix <- !test
-  txt_hpar <-  paste0("<p> A new version of HPA is available: <br> " , paste0(names(mat["hpaRelease",ix]),": ",
-                      mat["hpaRelease",ix], collapse = " <br>"), "</p>")
-}
-
-stquot <- statquote()
-
-# adding text
-email <- email %>% html(paste(txt_hpar, "<b>And the statistical quote a the day: </b> ", stquot$text))
-
-
-smtp <- server(host = "outlook.office365.com",
-               port = 587,
-               username = "manon.martin@uclouvain.be",
-               password = "MoabArches2016")
-smtp(email, verbose = TRUE)
+  
+  load(file.path("data","hpaRelease.rda"))
+  
+  ### testing hpar version
+  
+  getHpaRelease <- function() {
+    rel <- readLines("http://www.proteinatlas.org/about/releases", warn = FALSE)
+    suppressWarnings(reldate <- grep("Release date:", rel, value = TRUE)[1])
+    suppressWarnings(relver <- grep("Protein Atlas version", rel, value = TRUE)[1])
+    reldate <- sub("</b.+$", "", sub("^.+<b>", "", reldate))
+    relver <- sub("<.+$", "", sub("^.+version ", "", relver))
+    hpa <- c("version" = relver, "date" = reldate)
+    ens <- grep("Ensembl version", rel, value = TRUE, useBytes=TRUE)[1]
+    ens <- sub("</b>", "", sub("^.+<b>", "", ens))
+    ens <- sub("\t", "", ens)
+    ans <- c(hpa, ensembl = ens)
+    return(ans)
+  }
+  
+  HPARel <- getHpaRelease()
+  
+  mat <- rbind(HPARel, inst_package = c(hpaRelease["version"], 
+                                        hpaRelease["date"],
+                                        hpaRelease["ensembl"]))
+  
+  ### is hpar up to date?
+  
+  test <- apply(mat,2, duplicated)[2,]
+  
+  if (sum(test)==3){
+    txt_hpar <- "<p> Everything is up-to-date for <code> hpar </code> </p>"
+  } else{
+    ix <- !test
+    txt_hpar <-  paste0("<p> A new version of HPA is available: <br> " , paste0(names(mat["hpaRelease",ix]),": ",
+                        mat["hpaRelease",ix], collapse = " <br>"), "</p>")
+  }
+  
+  txt_hpar
